6c0e32af24f11d95d8fabc1c931ec3514a34e3b2
Geetanjali <geet2anju@gmail.com>
2016-02-02 15:20:29 +0530

    Added Classes for Tables

---
+++ src/main/java/com/projectManagement/UserProfile.java
@@ -0,0 +1,23 @@
+@Entity
+@Table(name = "USER_PROFILES")
+public class UserProfile {
+   	
+	//@Id @GeneratedValue
+	@OneToOne
+    	@JoinColumn(name = "USER_ID")
+	private User user;
+	
+	@Column(name = "PHONE_NUMBER")
+	private int phoneNumber;
+
+	
+	
+	public int getPhoneNumber() {
+      		return phoneNumber;
+   	}
+   	
+	public void setPhoneNumber( int phoneNumber ) {
+      		this.phoneNumber = phoneNumber;
+   	}
+
+}
