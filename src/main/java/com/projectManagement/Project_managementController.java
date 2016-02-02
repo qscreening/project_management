29990b6a44af27912310b2b1e86c0803043b829d
ror <priya.siddana@gmail.com>
2016-02-02 16:09:21 +0530

    Created servlets

---
+++ src/main/java/com/projectManagement/Project_managementController.java
@@ -0,0 +1,25 @@
+package com.projectManagement;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.hibernate.Session;
+import java.util.List;
+import org.hibernate.Query;
+ 
+
+@RestController
+public class Project_managementController {
+	@RequestMapping("/project_managementController")
+	public List management() {
+		Session session = HibernateSessionManager.getSessionFactory().openSession();
+		session.beginTransaction();
+		Query query = session.createQuery("from USERS");
+		List users = query.list();
+		session.save(users);
+		session.getTransaction().commit();
+		return users;
+	}
+}
+
+
