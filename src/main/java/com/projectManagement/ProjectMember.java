c0641ee9b0b53e3f0c76e57a46adf0cfb94ec8da
Chilvery Kartheek <chilvery.kartheek@gmail.com>
2016-02-03 12:05:42 +0530

    Indentation

--- src/main/java/com/projectManagement/ProjectMember.java
+++ src/main/java/com/projectManagement/ProjectMember.java
@@ -9,20 +9,18 @@ public class ProjectMember {
 	@Id @GeneratedValue
   	@Column(name = "ID")
   	private int id;	
-	@Column(name = "MEMBER_ID")
-	private int memberId;
-	@OneToOne
-    	@JoinColumn(name = "USER_ID")
-	private User user;
 	@OneToMany
     	@JoinColumn(name = "PROJECT_ID")
 	private Project project;	
 	/*@Column(name = "PROJECT_ID")
 	private int projectId;*/
-	
+	@Column(name = "MEMBER_ID")
+	private int memberId;
+	@OneToOne
+    	@JoinColumn(name = "USER_ID")
+	private User user;
 	@Column(name = "OWNER")
 	private boolean owner;
-
 	
 	public int getId() {
       		return id;
@@ -40,5 +38,4 @@ public class ProjectMember {
       		this.owner = owner;
    	}
 	
-
 }
