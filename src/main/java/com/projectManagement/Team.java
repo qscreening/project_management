9d3a4ee98d4b6747202763603a27257d0efe84a6
Chilvery Kartheek <chilvery.kartheek@gmail.com>
2016-02-03 12:06:19 +0530

    Created a Class

---
+++ src/main/java/com/projectManagement/Team.java
@@ -0,0 +1,35 @@
+package com.projectManagement;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "TEAMS")
+public class Teams {
+   	
+	@Id @GeneratedValue
+	@Column(name = "TEAM_ID")
+  	private int teamId;	
+	@Column(name = "TEAM_NAME")
+	private String teamName;
+	@Column(name = "TEAM_LEADER")
+	private int teamLeader;
+	@OneToOne
+    	@JoinColumn(name = "USER_ID")
+	private User user;
+
+	public int getTeamId() {
+      		return teamtId;
+   	}
+   	
+	public void setTeamId( int teamId ) {
+      		this.teamId = teamId;
+   	}
+	
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+}
