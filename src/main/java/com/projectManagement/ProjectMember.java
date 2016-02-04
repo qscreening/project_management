45b80c84c0e9eccf53d8e1471e8d44472671f626
Geetanjali <geet2anju@gmail.com>
2016-02-04 16:02:43 +0530

    Modified

--- src/main/java/com/projectManagement/ProjectMember.java
+++ src/main/java/com/projectManagement/ProjectMember.java
@@ -1,26 +1,39 @@
 package com.projectManagement;
 
-import java.util.Set;
+import java.util.List;
+
+import java.io.Serializable;
 
 import javax.persistence.*;
 
-@Embeddable
+@Entity
 @Table(name = "PROJECT_MEMBERS")
-public class ProjectMember {
+public class ProjectMember implements Serializable {
    	
 	@Id @GeneratedValue
   	@Column(name = "ID")
   	private int id;	
-	@OneToMany
-    	@JoinColumn(name = "PROJECT_ID")
-	private Set<Project> project;	
+	//@OneToMany
+    	@Column(name = "PROJECT_ID")
+	private int projectId;	
 	@Column(name = "MEMBER_ID")
 	private int memberId;
-	@OneToOne
-    	@JoinColumn(name = "USER_ID")
-	private User user;
+	//@OneToOne
+    	//@JoinColumn(name = "USER_ID")
+	//private User user;
 	@Column(name = "OWNER")
 	private boolean owner;
+
+	public 	ProjectMember() {
+ 
+    	}
+ 
+    	public ProjectMember(int id, int projectId, int memberId, boolean owner) {
+        	this.id = id;
+        	this.projectId = projectId;
+        	this.memberId = memberId;
+        	this.owner = owner;
+    	}
 	
 	public int getId() {
       		return id;
@@ -30,6 +43,22 @@ public class ProjectMember {
       		this.id = id;
    	}
 	
+	public int getProjectId() {
+      		return projectId;
+   	}
+
+   	public void setProjectId( int projectId ) {
+      		this.projectId = projectId;
+   	}
+	
+	public int getMemberId() {
+      		return memberId;
+   	}
+
+   	public void setMemberId( int memberId ) {
+      		this.memberId = memberId;
+   	}
+
 	public boolean getOwner() {
       		return owner;
    	}
