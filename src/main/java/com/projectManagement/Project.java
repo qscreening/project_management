ea715b27b6516bd5066ccb10d2dcb01fb9a408cb
Geetanjali <geet2anju@gmail.com>
2016-02-04 14:45:59 +0530

    Modified Class

--- src/main/java/com/projectManagement/Project.java
+++ src/main/java/com/projectManagement/Project.java
@@ -1,21 +1,42 @@
 package com.projectManagement;
 
+import java.io.Serializable;
+
 import javax.persistence.*;
 
-@Embeddable
+@Entity
 @Table(name = "PROJECTS")
-public class Project {
+public class Project implements Serializable {
    	
-	@Id @GeneratedValue
+	@Id 
+	@GeneratedValue
 	@Column(name = "PROJECT_ID")
   	private int projectId;	
 	@Column(name = "PROJECT_NAME")
 	private String projectName;
 	@Column(name = "OWNER")
 	private int owner;
-	@OneToOne
-    	@JoinColumn(name = "USER_ID")
-	private User user;
+	//@OneToOne
+    	//@JoinColumn(name = "USER_ID")
+	//private User user;
+
+	public 	Project() {
+ 
+    	}
+ 
+    	public Project(int projectId, String projectName, int owner) {
+        	this.projectId = projectId;
+        	this.projectName = projectName;
+        	this.owner = owner;
+    	}
+	
+	public int getOwner() {
+      		return owner;
+   	}
+   	
+	public void setOwner( int owner ) {
+      		this.owner = owner;
+   	}
 
 	public int getProjectId() {
       		return projectId;
