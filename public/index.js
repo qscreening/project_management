941ef0d6294908137568c9d6e39cb22d000d7dc7
Kartheek2016 <chilvery.kartheek@gmail.com>
2016-02-09 17:56:30 +0530

    Modified id's

--- public/index.js
+++ public/index.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 
 	$("#signIn").click(function() {
-		var emailId = $("#userName").val();
+		var emailId = $("#userId").val();
 		var password = $("password").val();
 		var atpos = emailId.indexOf("@");
 		var dotpos = emailId.lastIndexOf(".");
 		if (atpos < 1 || ( dotpos - atpos < 6 )) {
-			alert("Please enter correct email ID");
-			$("#userName").focus();
+			alert("Please enter correct email ID (abcdef@xyz.com)");
+			$("#userId").focus();
 		}
 		if (password === null || password === "") {
 			alert("Please enter the password.");
@@ -20,6 +20,13 @@ $(document).ready(function() {
 	});
 
 	$("#signUp").click(function () {
+		var emailId = $("#emailId").val();
+		var atpos = emailId.indexOf("@");
+		var dotpos = emailId.lastIndexOf(".");
+		if (atpos < 1 || ( dotpos - atpos < 6 )) {
+			alert("Please enter correct email ID (abcdef@xyz.com)");
+			$("#emailId").focus();
+		}
 		var password = $("#SignUpPswd").val();
 		var confirmPassword = $("#confirmPassword").val();
 		var compare = password.localeCompare(confirmPassword);
