6682cfb82f45ea7d195a4cb71eb93d91a46ea70f
Kartheek2016 <chilvery.kartheek@gmail.com>
2016-02-12 16:44:14 +0530

    Created a Object

--- src/main/java/com/projectManagement/Controller/SignInController.java
+++ src/main/java/com/projectManagement/Controller/SignInController.java
@@ -7,29 +7,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.servlet.ServletException;
 
-import java.io.IOException;  
+import java.util.List;
+import java.util.ArrayList;
 
 @RestController
 public class SignInController {
 
 	@RequestMapping(value ="/SignIn", method = RequestMethod.POST)
-	public boolean signin(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public int signin(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
 		String emailId = request.getParameter("userId");
 		String password = request.getParameter("Password");
+		List<User> list = new ArrayList<User>();
 
 		User user = new User();
 		SignInDao signInDao = new SignInDao();
 		boolean result = signInDao.signInUserDetails(emailId, password);
 
-		HttpSession session = request.getSession(true);  
-		session.setAttribute("userId",user.getId());
+		UserDao dao = new UserDao();
+		list = dao.getUserObj(emailId);
 
-		return result; 
+		Authorization auth = new Authorization();
+		int curUser = auth.checkOrCreateSession(request, list);
 
+		return curUser;
 	}
 
 }
