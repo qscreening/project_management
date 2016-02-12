9bacbdb864b894ee953d3661408dccd6da789337
Kartheek2016 <chilvery.kartheek@gmail.com>
2016-02-12 16:47:47 +0530

    Assigned resp user id as attribute

--- src/main/java/com/projectManagement/Library/Authorization.java
+++ src/main/java/com/projectManagement/Library/Authorization.java
@@ -8,19 +8,17 @@ import javax.servlet.ServletException;
 import java.util.List;
 import java.util.ArrayList;
 
-//import com.projectManagement.User;
-
 public class Authorization{
 
         private HttpSession session = null;
-
+	private int curUser = 0;
 
 	public static int sessionUser(){
 		return 1;
 	}
 
 	public int checkOrCreateSession(HttpServletRequest request, List<User> list) throws ServletException {
-		int curUser = 0;
+
 		int userId = list.get(0).getId();
 		if(session == null){
 			session = request.getSession(true);
@@ -30,6 +28,9 @@ public class Authorization{
 		}else {
 			System.out.println("session is Created");
 		}
+
 		return curUser;
+
 	}
+
 }
