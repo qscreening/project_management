258f78cbfe59d83e8a8474b0495f671fb2f890bf
Kartheek2016 <chilvery.kartheek@gmail.com>
2016-02-12 16:44:42 +0530

    Created a New method

--- src/main/java/com/projectManagement/Model/UserDao.java
+++ src/main/java/com/projectManagement/Model/UserDao.java
@@ -2,6 +2,7 @@ package com.projectManagement;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.lang.StringBuilder;
 
 import org.hibernate.Session;  
 import org.hibernate.Transaction;  
@@ -11,9 +12,10 @@ import org.hibernate.FetchMode;
 
 public class UserDao{
 
+	List<User> list = new ArrayList<User>();
+
 	public List<User> getListOfUsers(){
 
-		List<User> list = new ArrayList<User>();
 		Session session = HibernateSessionManager.getSessionFactory().openSession();
 		session.beginTransaction();
 		Transaction tx = null;       
@@ -35,6 +37,33 @@ public class UserDao{
 
 	        return list;
 
+	}
+
+	public List<User> getUserObj(String email){
+		
+		Session session = HibernateSessionManager.getSessionFactory().openSession();
+		session.beginTransaction();
+		Transaction tx = null;  
+
+		try {
+			tx = session.getTransaction();
+			tx.begin();
+			StringBuilder query = new StringBuilder();
+			email = query.append("from User where emailId = '").append(email).append("'").toString();
+			list = (List<User>) session.createQuery(email).list();
+			session.save(list);
+			tx.commit();
+	        } catch (Exception e) {
+			if (tx != null) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+	        } finally {
+			session.close();
+	        }
+
+	        return list;
+     
 	}
 
 }
