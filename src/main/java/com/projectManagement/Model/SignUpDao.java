5d9739d43660371cff8607d4ea95754cad54cae5
Geetanjali <geet2anju@gmail.com>
2016-02-15 12:21:10 +0530

    Added encryption method

--- src/main/java/com/projectManagement/Model/SignUpDao.java
+++ src/main/java/com/projectManagement/Model/SignUpDao.java
@@ -6,6 +6,8 @@ import org.hibernate.Query;
 import org.hibernate.Criteria;
 import org.hibernate.FetchMode;
 
+import java.security.*;
+
 public class SignUpDao{
 
 	public boolean signUpUserDetails(String fullName, String email, String password){  
@@ -21,7 +23,7 @@ public class SignUpDao{
 			User user = new User();
 			user.setUserName(fullName);
 			user.setEmailId(email);
-			user.setPassword(password);
+			user.setPassword(encryptPassword(password));
 			
 			session.save(user);
 			tx.commit();
@@ -37,4 +39,31 @@ public class SignUpDao{
 
 		return false;
 	}
+	
+	public String encryptPassword(String password) {
+        	String passwordToHash = password;
+        	String generatedPassword = null;
+        	try {
+            		// Create MessageDigest instance for MD5
+            		MessageDigest md = MessageDigest.getInstance("MD5");
+            		//Add password bytes to digest
+            		md.update(passwordToHash.getBytes());
+            		//Get the hash's bytes 
+            		byte[] bytes = md.digest();
+            		//This bytes[] has bytes in decimal format;
+            		//Convert it to hexadecimal format
+            		StringBuilder sb = new StringBuilder();
+            		for(int i=0; i< bytes.length ;i++)
+            		{
+                		sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            		}
+            		//Get complete hashed password in hex format
+            		generatedPassword = sb.toString();
+        	} 
+        	catch (NoSuchAlgorithmException e) {
+            		e.printStackTrace();
+        	}
+        	return generatedPassword;
+    	}
+
 }
