1ede024b2df202c2cc3cb44548ac8d66c72ea933
Kartheek2016 <chilvery.kartheek@gmail.com>
2016-02-15 18:12:49 +0530

    Creating a session

--- src/main/java/com/projectManagement/Controller/SignUpController.java
+++ src/main/java/com/projectManagement/Controller/SignUpController.java
@@ -8,20 +8,33 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.List;
+import java.util.ArrayList;
+
 @RestController
 public class SignUpController {
 
 	@RequestMapping(value = "/SignUp", method = RequestMethod.POST)
-	public boolean signUp(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public int signUp(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
 		String fullName = request.getParameter("fName");
 		String emailId = request.getParameter("email");
 		String password = request.getParameter("pswd");
+		List<User> list = new ArrayList<User>();
+		int curUser = 0;
 
 		SignUpDao signUpDao = new SignUpDao();
-		boolean status = signUpDao.signUpUserDetails(fullName, emailId, password);
+		boolean isCreated = signUpDao.signUpUserDetails(fullName, emailId, password);
 
-		return status;
+		if(isCreated){
+			UserDao dao = new UserDao();
+			list = dao.getUserObj(emailId);
+
+			Authorization auth = new Authorization();
+			curUser = auth.checkOrCreateSession(request, list);
+		}
+
+		return curUser;
 
 	}
 
