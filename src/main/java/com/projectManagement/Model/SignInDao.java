e050cc479a9bd3136aaedd32f8e9c170f8df1201
Kartheek2016 <chilvery.kartheek@gmail.com>
2016-02-16 10:46:28 +0530

    Indentation

--- src/main/java/com/projectManagement/Model/SignInDao.java
+++ src/main/java/com/projectManagement/Model/SignInDao.java
@@ -48,30 +48,26 @@ public class SignInDao{
 	}
 
 	public String encryptPassword(String password) {
-        	
+
 		String passwordToHash = password;
-        	String generatedPassword = null;
-        	try {
-            		// Create MessageDigest instance for MD5
-            		MessageDigest md = MessageDigest.getInstance("MD5");
-            		//Add password bytes to digest
-            		md.update(passwordToHash.getBytes());
-            		//Get the hash's bytes 
-            		byte[] bytes = md.digest();
-            		//This bytes[] has bytes in decimal format;
-            		//Convert it to hexadecimal format
-            		StringBuilder sb = new StringBuilder();
-            		for(int i=0; i< bytes.length ;i++)
-            		{
-                		sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            		}
-            		//Get complete hashed password in hex format
-            		generatedPassword = sb.toString();
-        	} 
-        	catch (NoSuchAlgorithmException e) {
-            		e.printStackTrace();
-        	}
-        	return generatedPassword;
+		String generatedPassword = null;
+
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");// Create MessageDigest instance for MD5
+			md.update(passwordToHash.getBytes());//Add password bytes to digest
+			byte[] bytes = md.digest();//Get the hash's bytes 
+			StringBuilder sb = new StringBuilder();//This bytes[] has bytes in decimal format; Convert it to hexadecimal format
+			for(int i=0; i< bytes.length ;i++){
+				sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			generatedPassword = sb.toString();//Get complete hashed password in hex format
+		}
+		catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
+		return generatedPassword;
+
     	}
 
 }
